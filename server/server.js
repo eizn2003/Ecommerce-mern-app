ae9ebe270e6dde47b73de0ea76f622aab29c94c1
eizn2003 <studizere@gmail.com>
2026-09-03 17:59:49 +0200

    added production link to allowed origins

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://trendorarw.vercel.app"];
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebHooks)
 
